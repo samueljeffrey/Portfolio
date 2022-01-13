6791db6fe7777b0228fd551529693b59279bf9f7
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2022-01-13 11:29:41 +0000

    add completed about section

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import {
   Container,
   Nav,
   Navbar,
+  Image,
+  Row,
+  Col,
 } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <section>
-        <Navbar fixed="top" bg="light" expand="lg">
+      <header>
+        <Navbar fixed="top" bg="light">
           <Container>
-            <Navbar.Brand href="#about">Samuel Jeffrey</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+              <Nav className="m-auto">
                 <Nav.Link className="nav-option" href="#about">
                   About
                 </Nav.Link>
@@ -35,257 +37,55 @@ function App() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-      </section>
+      </header>
 
       <section id="about">
-        <CardGroup className="card-group">
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/moneymethods.png"
-            />
-            <Card.Title>Money Methods</Card.Title>
-            <Card.Subtitle className="text-muted">
-              Vanilla Javascript
-            </Card.Subtitle>
-            <Card.Text>
-              Prior to my bootcamp, after around 1 month of self-teaching, I
-              created this site - using only HTML, CSS and JavaScript - which
-              allows users to calculate net income or compound interest, as well
-              as use a visual budget tool.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-success"
-                href="https://samuel-jeffrey-money-methods.netlify.app/"
-                target="_blank"
-              >
-                Website
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/MoneyMethods"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-            </Container>
-          </Card>
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/ncnews.png"
-            />
-            <Card.Title>NC News</Card.Title>
-            <Card.Subtitle className="text-muted">
-              React &#38; SQL
-            </Card.Subtitle>
-            <Card.Text>
-              My main solo project on the Northcoders Bootcamp is a news website
-              which allows users to sort, read, post, like and comment on
-              articles. I built the back-end API with a PostgreSQL database and
-              the front-end UI uses React.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-success"
-                href="https://samuel-jeffrey-nc-news.netlify.app/"
-                target="_blank"
-              >
-                Website
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/nc-news-front-end"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-              <Button
-                className="card-button btn-primary"
-                href="https://github.com/samueljeffrey/nc-news-back-end"
-                target="_blank"
-              >
-                Back End Code
-              </Button>
-            </Container>
-          </Card>
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/parkify.png"
-            />
-            <Card.Title>Parkify</Card.Title>
-            <Card.Subtitle className="text-muted">
-              React Native &#38; MongoDB
-            </Card.Subtitle>
-            <Card.Text>
-              For my group project at the end of the Northcoders Bootcamp, we
-              created a mobile app for organising events in parks, with a live
-              chat feature. We learned MongoDB and React Native to create what
-              we had envisioned.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-danger"
-                href="https://www.youtube.com/watch?v=-bu5VTr6Y3k"
-                target="_blank"
-              >
-                Presentation
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/parkify-front-end"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-              <Button
-                className="card-button btn-primary"
-                href="https://github.com/samueljeffrey/parkify-back-end"
-                target="_blank"
-              >
-                Back End Code
-              </Button>
-            </Container>
-          </Card>
-        </CardGroup>
+        <h1 id="about-title">About</h1>
+        <Container>
+          <Row>
+            <Col>
+              <Image src="./portrait.jpg" roundedCircle={true} id="portrait" />
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Samuel Jeffrey</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Manchester, UK
+                  </Card.Subtitle>
+                  <Card.Text>
+                    I'm a fresh Northcoders Bootcamp graduate looking for my
+                    first role in Software Development.
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    className="about-button"
+                    href="https://www.linkedin.com/in/samuel-jeffrey-123bb2226/"
+                    target="_blank"
+                  >
+                    LinkedIn
+                  </Button>
+                  <Button
+                    variant="dark"
+                    className="about-button"
+                    href="https://github.com/samueljeffrey"
+                    target="_blank"
+                  >
+                    GitHub
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </section>
 
       <section id="skills">
-        <CardGroup className="card-group">
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/moneymethods.png"
-            />
-            <Card.Title>Money Methods</Card.Title>
-            <Card.Subtitle className="text-muted">
-              Vanilla Javascript
-            </Card.Subtitle>
-            <Card.Text>
-              Prior to my bootcamp, after around 1 month of self-teaching, I
-              created this site - using only HTML, CSS and JavaScript - which
-              allows users to calculate net income or compound interest, as well
-              as use a visual budget tool.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-success"
-                href="https://samuel-jeffrey-money-methods.netlify.app/"
-                target="_blank"
-              >
-                Website
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/MoneyMethods"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-            </Container>
-          </Card>
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/ncnews.png"
-            />
-            <Card.Title>NC News</Card.Title>
-            <Card.Subtitle className="text-muted">
-              React &#38; SQL
-            </Card.Subtitle>
-            <Card.Text>
-              My main solo project on the Northcoders Bootcamp is a news website
-              which allows users to sort, read, post, like and comment on
-              articles. I built the back-end API with a PostgreSQL database and
-              the front-end UI uses React.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-success"
-                href="https://samuel-jeffrey-nc-news.netlify.app/"
-                target="_blank"
-              >
-                Website
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/nc-news-front-end"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-              <Button
-                className="card-button btn-primary"
-                href="https://github.com/samueljeffrey/nc-news-back-end"
-                target="_blank"
-              >
-                Back End Code
-              </Button>
-            </Container>
-          </Card>
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/parkify.png"
-            />
-            <Card.Title>Parkify</Card.Title>
-            <Card.Subtitle className="text-muted">
-              React Native &#38; MongoDB
-            </Card.Subtitle>
-            <Card.Text>
-              For my group project at the end of the Northcoders Bootcamp, we
-              created a mobile app for organising events in parks, with a live
-              chat feature. We learned MongoDB and React Native to create what
-              we had envisioned.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-danger"
-                href="https://www.youtube.com/watch?v=-bu5VTr6Y3k"
-                target="_blank"
-              >
-                Presentation
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/parkify-front-end"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-              <Button
-                className="card-button btn-primary"
-                href="https://github.com/samueljeffrey/parkify-back-end"
-                target="_blank"
-              >
-                Back End Code
-              </Button>
-            </Container>
-          </Card>
-        </CardGroup>
+        <h1>Skills</h1>
       </section>
 
       <section id="projects">
+        <h1>Projects</h1>
         <CardGroup className="card-group">
           <Card bg="light" className="card">
             <Card.Img
@@ -295,7 +95,7 @@ function App() {
             />
             <Card.Title>Money Methods</Card.Title>
             <Card.Subtitle className="text-muted">
-              Vanilla Javascript
+              Vanilla JavaScript
             </Card.Subtitle>
             <Card.Text>
               Prior to my bootcamp, after around 1 month of self-teaching, I
@@ -410,127 +210,7 @@ function App() {
       </section>
 
       <section id="contact">
-        <CardGroup className="card-group">
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/moneymethods.png"
-            />
-            <Card.Title>Money Methods</Card.Title>
-            <Card.Subtitle className="text-muted">
-              Vanilla Javascript
-            </Card.Subtitle>
-            <Card.Text>
-              Prior to my bootcamp, after around 1 month of self-teaching, I
-              created this site - using only HTML, CSS and JavaScript - which
-              allows users to calculate net income or compound interest, as well
-              as use a visual budget tool.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-success"
-                href="https://samuel-jeffrey-money-methods.netlify.app/"
-                target="_blank"
-              >
-                Website
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/MoneyMethods"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-            </Container>
-          </Card>
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/ncnews.png"
-            />
-            <Card.Title>NC News</Card.Title>
-            <Card.Subtitle className="text-muted">
-              React &#38; SQL
-            </Card.Subtitle>
-            <Card.Text>
-              My main solo project on the Northcoders Bootcamp is a news website
-              which allows users to sort, read, post, like and comment on
-              articles. I built the back-end API with a PostgreSQL database and
-              the front-end UI uses React.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-success"
-                href="https://samuel-jeffrey-nc-news.netlify.app/"
-                target="_blank"
-              >
-                Website
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/nc-news-front-end"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-              <Button
-                className="card-button btn-primary"
-                href="https://github.com/samueljeffrey/nc-news-back-end"
-                target="_blank"
-              >
-                Back End Code
-              </Button>
-            </Container>
-          </Card>
-          <Card bg="light" className="card">
-            <Card.Img
-              variant="top"
-              className="card-logo"
-              src="./logos/parkify.png"
-            />
-            <Card.Title>Parkify</Card.Title>
-            <Card.Subtitle className="text-muted">
-              React Native &#38; MongoDB
-            </Card.Subtitle>
-            <Card.Text>
-              For my group project at the end of the Northcoders Bootcamp, we
-              created a mobile app for organising events in parks, with a live
-              chat feature. We learned MongoDB and React Native to create what
-              we had envisioned.
-            </Card.Text>
-            <Container>
-              <Button
-                className="card-button btn-danger"
-                href="https://www.youtube.com/watch?v=-bu5VTr6Y3k"
-                target="_blank"
-              >
-                Presentation
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                className="card-button btn-warning"
-                href="https://github.com/samueljeffrey/parkify-front-end"
-                target="_blank"
-              >
-                Front End Code
-              </Button>
-              <Button
-                className="card-button btn-primary"
-                href="https://github.com/samueljeffrey/parkify-back-end"
-                target="_blank"
-              >
-                Back End Code
-              </Button>
-            </Container>
-          </Card>
-        </CardGroup>
+        <h1>Contact</h1>
       </section>
     </div>
   );
